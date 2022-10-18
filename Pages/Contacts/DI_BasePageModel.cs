b5bb02d30bb1f8cb94ee9e7acab16c363be53734
Gary <Gary.Dix@enform.nz>
2022-10-18 15:35:19 +1300

    updating

--- Pages/Contacts/DI_BasePageModel.cs
+++ Pages/Contacts/DI_BasePageModel.cs
@@ -12,18 +12,15 @@ namespace Authorizattion_exercise.Pages.Contacts
         protected ApplicationDbContext Context { get; }
         protected IAuthorizationService AuthorizationService { get; }
         protected UserManager<IdentityUser> UserManager { get; }
-       
 
         public DI_BasePageModel(
             ApplicationDbContext context,
             IAuthorizationService authorizationService,
-            UserManager<IdentityUser> userManager
-            ) : base()
+            UserManager<IdentityUser> userManager) : base()
         {
             Context = context;
             UserManager = userManager;
             AuthorizationService = authorizationService;
-            
         }
     }
 }
